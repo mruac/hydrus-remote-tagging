489b93390020cfb822fc61cc173808a15a4831b0
koto <koto@koto.reisen>
2019-10-06 18:49:04 -0400

    beta swipe functionality

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ flask
 Just run `$ python server.py`  
 
 **You also might want to change the app.secret_key in the server.py file**
+
+## Keybinds  
+A - Archive 
+S - Skip  
+D - Delete  
+Z - Go back to previous page  
+
+## Swiping  
+You can swipe in a direction instead of pressing the buttons or keybinds.  
+Swipe right to archive  
+Swipe left to delete  
+Swipe up to skip  
--- templates/static/show.js
+++ templates/static/show.js
@@ -2,38 +2,65 @@ var current = location.href.split('/')[4].replace(/\?.*$/, '');
 var next = parseInt(current) + 1
 var prev = parseInt(current) - 1
 if ($('h1').hasClass('nm-text')) {
-	window.location = next
+    window.location = next
 } else {
-	$("a#a-d-button").click(function() {
-	    var hAction = this.getAttribute("value");
-		$.post(current, {action: hAction}, function(){
-			window.location = next
-		});
-	});
-	$(document).keydown(function(e) {
-	    switch(e.which) {
-	        case 65: // a
-		        $.post(current, {action: "archive"}, function(){
-					window.location = next
-				});
-		        break;
+    $("a#a-d-button").click(function() {
+        var hAction = this.getAttribute("value");
+        $.post(current, { action: hAction }, function() {
+            window.location = next
+        });
+    });
+    $(document).keydown(function(e) {
+        switch (e.which) {
+            case 65: // a
+                $.post(current, { action: "archive" }, function() {
+                    window.location = next
+                });
+                break;
 
-	        case 68: // d
-	        	$.post(current, {action: "delete"}, function(){
-					window.location = next
-				});
-	        	break;
+            case 68: // d
+                $.post(current, { action: "delete" }, function() {
+                    window.location = next
+                });
+                break;
 
-	        case 83: // s
-				window.location = next
-				break;
+            case 83: // s
+                window.location = next
+                break;
 
-	        case 90: // z
-				window.location = prev
-				break;
+            case 90: // z
+                window.location = prev
+                break;
 
-	        default: return;
-	    }
-	    e.preventDefault();
-	});
+            default:
+                return;
+        }
+        e.preventDefault();
+    });
 }
+$(function() {
+    //Enable swiping...
+    $("main").swipe({
+        //Generic swipe handler for all directions
+        swipe: function(event, direction, distance, duration) {
+            $(this).css("transition-duration", (duration / 1000).toFixed(1) + "s");
+            if (direction == 'right') {
+                $(this).css("transform", "translate(50%,0)");
+                $.post(current, { action: "archive" }, function() {
+                    window.location = next
+                });
+            } else if (direction == 'left') {
+                $(this).css("transform", "translate(-50%,0)");
+                $.post(current, { action: "delete" }, function() {
+                    window.location = next
+                });
+            } else if (direction == 'up') {
+                window.location = next
+                $(this).css("transform", "translate(0,-50%)");
+            }
+
+        },
+        threshold: 100,
+        triggerOnTouchEnd: true
+    });
+});
